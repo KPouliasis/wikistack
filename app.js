50943cdabd0f6ba193359b36fad84ed3b6f7abc5
existentialtype <konstantinos.pouliasis@gmail.com>
2017-01-23 13:14:43 -0500

    changes on rendering

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ const nunjucks = require('nunjucks');
 
 const bodyParser = require('body-parser');
 
+
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views',{nocache : true});
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use('/wiki', theRouter);
-
-
 
 var syncingPage = models.User.sync({})
 .then(function() {
@@ -23,13 +28,4 @@ var syncingPage = models.User.sync({})
   app.listen(3000, () => console.log('server is listening on port 3000'));
 })
 .catch(console.error);
-
-
-
-
-
-
-
 // models.db.sync({force: true});
-
-
--- routes/wiki.js
+++ routes/wiki.js
@@ -2,17 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const pg = require('pg');
-const pgConnect = new pg.Client();
-const wikistackDB = pgConnect('postgres://localhost:5432/wikistack');
+const nj = require('nunjucks')
+// const pgConnect = new pg.Client();
+// const wikistackDB = pgConnect('postgres://localhost:5432/wikistack');
 
 
 router.get('/', function(req, res, next) {
-  console.log(wikistackDB);
-  res.send('hello');
+  res.redirect('/')
 });
 
-router.post('/', function() {})
+// router.post('/', function() {})
 
-router.get('/add/', function() {})
+router.get('/add/', function(req, res) {
+  let object = {authorName:'Kostass',authorEmail:'k@aol.com'}
+   nj.render('../views/addpage.html',object)
+})
+
 
 module.exports = router;
